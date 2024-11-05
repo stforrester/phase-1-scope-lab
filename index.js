88e89719de86d68e781228b32ae0a7af0698f775
Stuart Forrester <stforrester@gmail.com>
2024-11-05 01:58:35 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName =`bob`;
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'definitely bob';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'we\'re no longer so sure it\'s bob';
+}
+
